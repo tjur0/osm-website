eac4cb9cb675994760c37cf45b2ab160da60dc37
Tjur0 <tjur0@users.noreply.github.com>
2025-04-30 21:25:06 +0200

    include sqlite

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
+  outputFileTracingIncludes: {
+    "./": ["**/*.sqlite*"],
+  },
 };
 
 export default nextConfig;
