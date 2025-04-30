f5890779fda923f975244cd3eb3fc0d8c33d3ec7
Tjur0 <tjur0@users.noreply.github.com>
2025-04-30 21:38:06 +0200

    enable the log

--- frontend/static/db.ts
+++ frontend/static/db.ts
@@ -5,12 +5,12 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const dbPath = path.join(__dirname, "pois.sqlite");
+const dbPath = path.join(__dirname, "./pois.sqlite");
 
 const { default: BetterSqlite3 } = await import("better-sqlite3");
 
 const db = new BetterSqlite3(dbPath, {
-  // verbose: console.log,
+  verbose: console.log,
   readonly: true,
 });
 
