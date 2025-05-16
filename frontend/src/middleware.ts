064d0b370fb2d9d9831fa372d6a35ceba2246994
Tjur0 <tjur0@users.noreply.github.com>
2025-05-16 12:01:23 +0200

    remove timstamp

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  console.log(
-    `[${new Date().toISOString()}] ${req.method} ${req.nextUrl.pathname}`,
-  );
+  console.log(`${req.method} ${req.nextUrl.pathname}`);
   return NextResponse.next();
 }
