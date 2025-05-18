4d0e10ba7a77a8af4f0f398a712b3af98da0b4bc
Tjur0 <tjur0@users.noreply.github.com>
2025-05-18 12:59:01 +0200

    disable animation on zoom

--- frontend/src/components/implemented-map.tsx
+++ frontend/src/components/implemented-map.tsx
@@ -105,7 +105,7 @@ export default function ImplemtedMap() {
 
   useEffect(() => {
     if (clicked) return;
-    zoomToSelected(true);
+    zoomToSelected(false);
   }, [zoomToSelected, pathname]);
 
   useEffect(() => {
